44a19a200243ec25cd24d54b6fc8b97df3f98fe9
Nikita Landarenko <landarenko_nikita@mail.ru>
2024-04-12 13:58:52 +0500

    Moved the screen switching logic into a fragment

--- app/src/main/java/com/example/ratemovie/presentation/MainActivity.kt
+++ app/src/main/java/com/example/ratemovie/presentation/MainActivity.kt
@@ -3,9 +3,6 @@ package com.example.ratemovie.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.example.ratemovie.R
 import com.example.ratemovie.databinding.MainScreenActivityBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,16 +19,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        binding.navView.setupWithNavController(navController)
-
-        binding.navView.setOnItemReselectedListener { item ->
-            val reselectedDestinationId = item.itemId
-            navController.popBackStack(reselectedDestinationId, inclusive = false)
-        }
     }
 }
