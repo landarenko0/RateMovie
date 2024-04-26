17af2239a5b20dd5cb3dada92178302b4c522323
Nikita Landarenko <landarenko_nikita@mail.ru>
2024-04-26 23:51:12 +0500

    Changed type to MutableList

--- app/src/main/java/com/example/ratemovie/domain/entities/User.kt
+++ app/src/main/java/com/example/ratemovie/domain/entities/User.kt
@@ -1,12 +1,30 @@
 package com.example.ratemovie.domain.entities
 
+import com.google.firebase.auth.FirebaseAuth
+
 data class User(
     val username: String,
     val email: String,
-    val liked: List<String>,
-    val reviewed: List<String>
-)
-{
-    constructor() : this("", "", emptyList(), emptyList())
-    constructor(username: String, email: String) : this(username, email, emptyList(), emptyList())
+    val liked: MutableList<String>,
+    val reviewed: MutableList<String>
+) {
+    val id
+        get() = FirebaseAuth.getInstance().currentUser?.uid
+            ?: throw RuntimeException("User is null")
+
+    constructor() : this("", "", mutableListOf(), mutableListOf())
+    constructor(username: String, email: String) : this(
+        username,
+        email,
+        mutableListOf(),
+        mutableListOf()
+    )
+
+    fun addMovieToFavorites(movieId: String) = liked.add(movieId)
+
+    fun deleteMovieFromFavorites(movieId: String) = liked.remove(movieId)
+
+    fun addMovieToReviewed(movieId: String) = reviewed.add(movieId)
+
+    fun deleteMovieFromReviewed(movieId: String) = reviewed.remove(movieId)
 }
