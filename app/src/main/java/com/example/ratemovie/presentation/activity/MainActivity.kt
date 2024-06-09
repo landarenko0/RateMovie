9c4a87cfdead429ef64ad2dc4ab1b6b95cbc033a
landarenko0 <landarenko_nikita@mail.ru>
2024-06-09 22:07:34 +0500

    Refactor

--- app/src/main/java/com/example/ratemovie/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/ratemovie/presentation/activity/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.ratemovie.presentation.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.ratemovie.R
 import com.example.ratemovie.databinding.MainScreenActivityBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,5 +24,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.navView.setupWithNavController(navController)
     }
 }
